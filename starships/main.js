5af55a21a7cedcf366f77f7184f51d90d2d7b0ce
Dylan <10767128@my.uvu.edu>
2021-03-18 12:45:11 -0600

    added a starfield, thanks THOR

--- starships/main.js
+++ starships/main.js
@@ -48,27 +48,27 @@ function populateShipView(shipData) {
 populateNav()
 
 
-// function addStarField(element, numStars) {
-//     element.style.setProperty('background-color', 'black')
-//     for (let i = 0; i < numStars; i++) {
-//         let star = document.createElement('div')
-//         star.style.setProperty('width', '2px')
-//         star.style.setProperty('height', '2px')
-//         star.style.setProperty('background-color', 'white')
-//         let xy = getRandomPosition()
-//         star.style.left = `${xy[0]}px`
-//         star.style.top = `${xy[1]}px`
-//         star.style.setProperty('position', 'absolute')
-//         element.appendChild(star)
-//     }
-// }
+function addStarField(element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i = 0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('width', '2px')
+        star.style.setProperty('height', '2px')
+        star.style.setProperty('background-color', 'white')
+        let xy = getRandomPosition()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${xy[1]}px`
+        star.style.setProperty('position', 'absolute')
+        element.appendChild(star)
+    }
+}
 
-// function getRandomPosition() {
-//     let y = document.body.scrollHeight
-//     let x = document.body.scrollWidth
-//     let randomY = Math.floor(Math.random() * y)
-//     let randomX = Math.floor(Math.random() * x)
-//     return [randomX, randomY]
-// }
+function getRandomPosition() {
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return [randomX, randomY]
+}
 
-// addStarField(document.querySelector('body'), 250)
+addStarField(document.querySelector('body'), 250)
