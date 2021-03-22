2d7460a2d3a55df807bf270ea71ba1a39e4b400f
Dylan <10767128@my.uvu.edu>
2021-03-22 13:58:40 -0600

    worked on a nav, to link all my starwarspages

--- characters-js/main.js
+++ characters-js/main.js
@@ -68,3 +68,35 @@ window.onscroll = function() {
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progress.style.height = progressHeight + "%";
 }
+
+
+// nav poggers
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+
+    burger.addEventListener('click', () => {
+        //toggle nav
+        nav.classList.toggle('nav-active');
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+            }
+        });
+        //burger animation
+        burger.classList.toggle('toggle');
+
+    });
+
+    
+}
+
+navSlide()
+
