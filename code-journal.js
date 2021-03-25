32e10e1644327e7ca1b49b60dd45146b4639a521
Dylan <10767128@my.uvu.edu>
2021-03-25 12:19:25 -0600

    started next project and udpated code journal

--- code-journal.js
+++ code-journal.js
@@ -52,12 +52,73 @@ let minObject = {};
 
 // Functions
 
-funciton myFunction() {
-    return 'my f in chat for ya';
-};
+// funciton myFunction() {
+//     return 'my f in chat for ya';
+// };
 
 function sumTwoThings(one, two) {
     return one + two;
 };
 
 const theFunction = () => 'I am awesome';
+
+//higher order function is a function that accepts another funcion as a parameter
+//filter, map and reduce are the most popular, but forEach, every, find, and some are also HOFs
+
+//filter method example, filter returns and array that passes the filter test
+
+
+const pilots = [
+    {
+      id: 2,
+      name: "Wedge Antilles",
+      faction: "Rebels"
+    },
+    {
+      id: 8,
+      name: "Ciena Ree",
+      faction: "Empire"
+    },
+    {
+      id: 40,
+      name: "Iden Versio",
+      faction: "Empire"
+    },
+    {
+      id: 66,
+      name: "Thane Kyrell",
+      faction: "Rebels"
+    }
+  ];
+  
+  const rebels = pilots.filter((pilot) => pilot.faction === "Rebels");
+  const empire = pilots.filter((pilot) => {
+    return pilot.faction === "Empire";
+  });
+  
+  // Array helper method 'map'
+  
+  const pilotsWithDate = pilots.map((pilot) => {
+    let date = new Date();
+    pilot.date = date.toLocaleDateString("en-US");
+    return pilot;
+  });
+  
+  let filmURLs = [
+    "https://swapi.co/api/films/",
+    "https://swapi.co/api/films/5/",
+    "https://swapi.co/api/films/4/this one is longer... even longer",
+    "https://swapi.co/api/films/6/",
+    "https: ",
+    "https://swapi.co/api/films/1/"
+  ];
+  
+  const filmLengths = filmURLs.map((filmURL) => filmURL.length);
+  
+  const filmPlusMore = filmURLs.map((filmURL) => {
+    let filmObj = {
+      index: filmURL,
+      foo: "Bar"
+    };
+    return filmObj;
+  });
