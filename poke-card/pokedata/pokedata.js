a63b3bee42e1205f0edc4e538ad97c61a7a29b82
Dylan <10767128@my.uvu.edu>
2021-03-31 16:38:25 -0600

    added my pokecard-project and added fontawsome.

---
+++ poke-card/pokedata/pokedata.js
@@ -0,0 +1,12 @@
+export const pokedex = [
+    {
+      name: "Pikachu",
+      type: "eletric",
+      evolution: "2",
+      card: "basic",
+      color: "yellow",
+      sex: "male",
+      gen: "first",
+      url: "http://pokeapi.co/api/v2/ability/31/",
+    },
+  ];
