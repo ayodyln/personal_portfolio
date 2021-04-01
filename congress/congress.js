4b167424743c527ad9025deafe722a8486bda7d7
Dylan <10767128@my.uvu.edu>
2021-04-01 10:45:46 -0600

    styled congress and remade my pokepage

--- congress/congress.js
+++ congress/congress.js
@@ -1,17 +1,22 @@
 import { senators } from '../data/senators.js'
 import { representatives } from '../data/representatives.js'
+import { removeChildren } from '../utility_functions/index.js'
 
 const congressGrid = document.querySelector('.congressGrid')
 const seniorityButton = document.querySelector('#seniorityButton')
 const birthdayButton = document.querySelector('#birthdayButton')
 
 function populateCongressDiv(simplifiedList) {
+    removeChildren(congressGrid)
     simplifiedList.forEach(person => {
         let personDiv = document.createElement('div')
         personDiv.className = 'figureDiv'
         let personFig = document.createElement('figure')
+        personFig.className = 'personFig'
         let figImg = document.createElement('img')
+        figImg.className = 'figImg'
         let figCaption = document.createElement('figcaption')
+        figCaption.className = 'figCap'
 
         figImg.src = person.imgURL
         figCaption.textContent = person.name
@@ -20,16 +25,18 @@ function populateCongressDiv(simplifiedList) {
         personFig.appendChild(figCaption)
         personDiv.appendChild(personFig)
         congressGrid.appendChild(personDiv)
+
     })
 }
 
 function getSimplifiedPeople(peopleList) {
+    
     return peopleList.map(person => {
         let middleName = person.middle_name ? ` ${person.middle_name}` : ``
         return {
             id: person.id,
             name: `${person.first_name}${middleName} ${person.last_name}`,
-            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-200px.jpeg`
         }
     })
 }
@@ -37,21 +44,21 @@ function getSimplifiedPeople(peopleList) {
 populateCongressDiv(getSimplifiedPeople(representatives))
 
 
-// const repubButton = document.querySelector('#republicans')
-// repubButton.addEventListener('click', () => {
-//     showRepublicans()
-// })
-// function showRepublicans() {
-//     //const repubs = representatives.filter(rep => rep.party === 'R')
-//     // TODO:  Looks like filter first then map would be best
-//     const repubs = representatives.map(rep => {
-//         let smallRepub = {}
-//         if (rep.party === 'R') {
-//                 smallRepub.id = rep.id
-//                 smallRepub.name = `${rep.first_name} ${rep.middle_name} ${rep.last_name}`
-//         }
-//         return smallRepub
-//     })
-//     console.log(repubs)
-// }
+const repubButton = document.querySelector('#republicans')
+repubButton.addEventListener('click', () => {
+    showRepublicans()
+})
+function showRepublicans() {
+    //const repubs = representatives.filter(rep => rep.party === 'R')
+    // TODO:  Looks like filter first then map would be best
+    const repubs = representatives.map(rep => {
+        let smallRepub = {}
+        if (rep.party === 'R') {
+                smallRepub.id = rep.id
+                smallRepub.name = `${rep.first_name} ${rep.middle_name} ${rep.last_name}`
+        }
+        return smallRepub
+    })
+    console.log(senators)
+}
 
