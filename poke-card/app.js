e8b9b49cd833f877b84b8b4ef6badcd96b67774f
Dylan <10767128@my.uvu.edu>
2021-04-02 18:55:56 -0600

    I swapped my poke project and updated some design

--- poke-card/app.js
+++ poke-card/app.js
@@ -1,45 +1,45 @@
-const pokedex = document.getElementById("pokedex");
+//Total Pokemon: 898; Target for assignmetn: 25
 
+// const fetchPokemon  = () => {
+//     const promises = []
+//     for (let i = 1; i <= 25; i++) {
+//         const url = `https://pokeapi.co/api/v2/pokemon/${i}`
+//         promises.push(fetch(url).then((res) => res.json()))
+//     }
+// }
 
-const fetchPokemon = () => {
-  const promises = [];
-  for (let i = 1; i <= 898; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
+const pokeGrid = document.querySelector(".pokeGrid");
+const loadButton = document.querySelector(".loadPoke");
+
+loadButton.addEventListener("click", () => {
+  loadPage();
+});
+
+async function getAPIData(url) {
+  try {
+    const response = await fetch(url); // try getting data from the API at the url provided
+    const data = await response.json(); // convert the response into JSON
+    return data; // return the data from the function to whoever called it
+  } catch (error) {
+    // must have been an error
+    console.log(error);
   }
-  
-  Promise.all(promises).then((results) => {
-    // console.log(results)
-    const pokemon = results.map((data) => ({
-      name: data.name,
-      id: data.id,
-      front_image: data.sprites["front_default"],
-      back_image: data.sprites["back_default"],
-      type: data.types.map((type) => type.type.name).join(", "),
-      
-    }));
-   
-    displayPokemon(pokemon);
-    console.log(pokemon)
+}
+
+function loadPage() {
+  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then((data) => {
+    for (const singlePokemon of data.results) {
+      populatePokeCard(singlePokemon);
+    }
   });
-};
+}
 
-const displayPokemon = (pokemon) => {
-  // console.log(pokemon)
-  const pokemonTHMLString = pokemon
-    .map(
-      (pokemon) => `
-    <li class = "poke-card">
-        <img class = "card-image" src ="${pokemon.front_image}"/>
-        <h2 class = "card-title">${pokemon.id}. ${pokemon.name}</h2>
-        <p class = "card-subtitle">Type: ${pokemon.type}</p>
-    </li>
-    `
-    )
-    .join("");
-  pokedex.innerHTML = pokemonTHMLString;
-};
+function populatePokeCard(singlePokemon) {
+  //use the same html as code example for assignment page for cardflip example
+  let pokeScene = document.createElement("div");
+  let pokeCard = document.createElement("div");
+  //make card front and back
+  //append them all to pokegrid
 
-fetchPokemon();
-
-//help from https://youtube.com/watch?v=T-VQUKeSU1w
+  console.log(singlePokemon);
+}
