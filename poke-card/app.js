23967d8b38e19a759b72ba936298c285f3689013
Dylan <10767128@my.uvu.edu>
2021-04-07 11:49:13 -0600

    poke project (imgs provied by fanzeyi on github

--- poke-card/app.js
+++ poke-card/app.js
@@ -4,22 +4,65 @@ import { removeChildren } from "../utility_functions/index.js";
 
 const pokeGrid = document.querySelector(".pokeGrid");
 
-const loadButton = document.querySelector(".loadPokemon");
-const genTwoButton = document.querySelector('.gentwo-button')
-const genThreeButton = document.querySelector('.genthree-button')
+
+
+//search bar
+
+let pokeSearch = []
+const searchBar = document.querySelector('#searchBar')
+
+searchBar.addEventListener('keyup', (e) => {
+  const searchString = e.target.value
+  const filteredPokemonSearch = pokeSearch.filter( (pokemon) => {
+    return (
+      pokemon.name.includes(searchString) || pokemon.id.includes(searchString)
+    )
+  })
+  console.log(filteredPokemonSearch)
+})
+
+//Buttons
+
+const loadButton = document.querySelector("#genone-button");
+const genTwoButton = document.querySelector('#gentwo-button')
+const genThreeButton = document.querySelector('#genthree-button')
+const genFourButton = document.querySelector('#genfour-button')
+const genFiveButton = document.querySelector('#genfive-button')
+const genSixButton = document.querySelector('#gensix-button')
+const genSevenButton = document.querySelector('#genseven-button')
+const genEightButton = document.querySelector('#geneight-button')
 
 loadButton.addEventListener("click", () => {
   loadPage();
 });
-
 genTwoButton.addEventListener('click', () => {
   genTwoPage();
 })
-
 genThreeButton.addEventListener('click', () => {
   genThreePage()
 })
+genFourButton.addEventListener('click', () => {
+  genFourPage()
+})
+genFiveButton.addEventListener('click', () => {
+  genFivePage()
+})
+genSixButton.addEventListener('click', () => {
+  genSixPage()
+})
+genSevenButton.addEventListener('click', () => {
+  genSevenPage()
+})
+// genEightButton.addEventListener('click', () => {
+//   genEightPage()
+// })
 
+//buttons end
+
+
+// document.location.reload(true)
+
+//Calling for api, awaiting resonse and return it as data, then functions for specific data ranges.
 
 async function getAPIData(url) {
   try {
@@ -45,7 +88,100 @@ function loadPage() {
   );
 };
 
+function genTwoPage() {
+  removeChildren(pokeGrid)
+  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=151`).then(
+    async (data) => {
+      for (const singlePokemon of data.results) {
+        await getAPIData(singlePokemon.url).then((pokeData) =>
+          populatePokeCard(pokeData)
+        );
+      }
+    }
+  );
+};
 
+function genThreePage() {
+  removeChildren(pokeGrid)
+  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=135&offset=251`).then(
+    async (data) => {
+      for (const singlePokemon of data.results) {
+        await getAPIData(singlePokemon.url).then((pokeData) =>
+          populatePokeCard(pokeData)
+        );
+      }
+    }
+  );
+}
+
+function genFourPage() {
+  removeChildren(pokeGrid)
+  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=107&offset=386`).then(
+    async (data) => {
+      for (const singlePokemon of data.results) {
+        await getAPIData(singlePokemon.url).then((pokeData) =>
+          populatePokeCard(pokeData)
+        );
+      }
+    }
+  );
+}
+
+function genFivePage() {
+  removeChildren(pokeGrid)
+  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=156&offset=493`).then(
+    async (data) => {
+      for (const singlePokemon of data.results) {
+        await getAPIData(singlePokemon.url).then((pokeData) =>
+          populatePokeCard(pokeData)
+        );
+      }
+    }
+  );
+}
+
+function genSixPage() {
+  removeChildren(pokeGrid)
+  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=72&offset=649`).then(
+    async (data) => {
+      for (const singlePokemon of data.results) {
+        await getAPIData(singlePokemon.url).then((pokeData) =>
+          populatePokeCard(pokeData)
+        );
+      }
+    }
+  );
+}
+
+function genSevenPage() {
+  removeChildren(pokeGrid)
+  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=88&offset=721`).then(
+    async (data) => {
+      for (const singlePokemon of data.results) {
+        await getAPIData(singlePokemon.url).then((pokeData) =>
+          populatePokeCard(pokeData)
+        );
+      }
+    }
+  );
+}
+
+// function genEightPage() {
+//   removeChildren(pokeGrid)
+//   getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=89&offset=809`).then(
+//     async (data) => {
+//       for (const singlePokemon of data.results) {
+//         await getAPIData(singlePokemon.url).then((pokeData) =>
+//           populatePokeCard(pokeData)
+//         );
+//       }
+//     }
+//   );
+// }
+
+//calling api section end
+
+//populating page section
 
 function populatePokeCard(singlePokemon) {
   // use the same html as in the CodePen Card flip example
@@ -67,24 +203,37 @@ function populatePokeCard(singlePokemon) {
 
 function populateCardFront(pokemon) {
   console.log(pokemon);
+
   let pokeFront = document.createElement("div");
   pokeFront.className = "card__face card__face--front";
-  let frontLabel = document.createElement("p");
+
+  let frontLabel = document.createElement("h3");
   frontLabel.textContent = pokemon.name;
+
   let frontImage = document.createElement("img");
   frontImage.className = 'front-img-pokemon'
   frontImage.src = `poke-img/${getImageFileName(pokemon)}.png`;
+
+  let pokemonID = document.createElement('h2');
+  pokemonID.className = ".pokeID";
+  pokemonID.textContent = pokemon.id
+
+  pokeFront.appendChild(pokemonID)
   pokeFront.appendChild(frontImage);
   pokeFront.appendChild(frontLabel);
+
   return pokeFront;
 }
 
 function populateCardBack(pokemon) {
   let pokeBack = document.createElement("div");
   pokeBack.className = "card__face card__face--back";
+
   let backLabel = document.createElement("p");
   backLabel.textContent = `Moves: ${pokemon.moves.length}`;
+
   pokeBack.appendChild(backLabel);
+
   return pokeBack;
 }
 
@@ -97,3 +246,7 @@ function getImageFileName(pokemon) {
     return `${pokemon.id}`
   }
 }
+
+//end of population section
+
+
